3c20a70485d5499b8696f9dd67d361a14ec3a32e
vimwitch <jchancehud@gmail.com>
2024-10-21 15:25:54 -0700

    wip: remove twenty-first dep

--- src/lib.rs
+++ src/lib.rs
@@ -48,8 +48,6 @@ mod custom;
 pub mod alt_bn128;
 #[cfg(feature = "curve25519")]
 pub mod curve_25519;
-#[cfg(all(feature = "oxfoi", target_pointer_width = "64"))]
-pub mod oxfoi;
 #[cfg(feature = "oxfoi")]
 pub mod oxfoi_slow;
 
@@ -61,9 +59,7 @@ pub use alt_bn128::Bn128FieldElement;
 #[cfg(feature = "curve25519")]
 pub use curve_25519::Curve25519FieldElement;
 pub use num_bigint::BigUint;
-#[cfg(all(feature = "oxfoi", target_pointer_width = "64"))]
-pub use oxfoi::OxfoiFieldElement;
-#[cfg(all(feature = "oxfoi", not(target_pointer_width = "64")))]
+#[cfg(all(feature = "oxfoi"))]
 pub use oxfoi_slow::OxfoiFieldElement;
 
 /// A generic representation of a scalar finite field element.
@@ -310,12 +306,6 @@ mod tests {
     #[test]
     fn sqrt_foi_slow() {
         test_sqrt::<oxfoi_slow::OxfoiFieldElement>();
-    }
-
-    #[cfg(not(target_arch = "wasm32"))]
-    #[test]
-    fn sqrt_foi() {
-        test_sqrt::<oxfoi::OxfoiFieldElement>();
     }
 
     #[test]
